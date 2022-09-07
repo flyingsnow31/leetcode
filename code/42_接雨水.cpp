d48100744fdb3fd51e8ba58f029b131edf5221da
LZY <1095609182@qq.com>
2022-09-07 12:59:24 +0800

    42接雨水

--- code/42_接雨水.cpp
+++ code/42_接雨水.cpp
@@ -1,4 +1,6 @@
-class Solution {
+#include "bits/stdc++.h"
+#include <vector>
+class Solution { //双指针
 public:
     int trap(vector<int>& height) {
         int left = 0, right = height.size() - 1;
@@ -15,6 +17,28 @@ public:
                 ans += rightmax - height[right];
                 right--;
             }
+        }
+        return ans;
+    }
+};
+class Solution2 { //dp
+public:
+    int trap(vector<int>& height) {
+        int len = height.size();
+        vector<int> left(len, 0), right(len, 0);
+        for(int i = 1; i < len ;i++) {
+            left[i] = max(left[i - 1], height[i - 1]); 
+            //cout<<left[i] <<" ";
+        }
+        //cout<<endl;
+        for(int i = len - 2; i >= 0; i--) {
+            right[i] = max(right[i + 1], height[i + 1]);
+            //cout<<right[i] <<" ";
+        }
+        //cout<<endl;
+        int ans = 0;
+        for(int i = 0; i < len ; i++) {
+            ans += max(0, min(left[i], right[i]) - height[i]);
         }
         return ans;
     }
